e80f963e92c324af63ee26a8ca6295820f89616c
Poofs <77813904+PoofyPloop@users.noreply.github.com>
2022-12-21 14:51:09 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-// I, Rawad Haddad (000777218), certify that this material is my original work. No other person's work has been used without due acknowledgement and I have not made my work available to anyone else.
+// @author PoofyPloop
 
 // LED states
 int LEDState = HIGH;
